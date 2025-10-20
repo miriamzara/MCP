74c3da584ae623702bed9c44441d384eb9cfcef0
Miriam Zara <miriamzara@MacBook-Pro-di-Miriam.local>
2025-10-20 18:59:05 +0200

    played with cuda basics

---
+++ cuda-c/basics.cpp
@@ -0,0 +1,103 @@
+// #include <stdio.h> // the io library of c
+#include <iostream> // the io library of c++
+#include <stdlib.h> // string manipulation - needed for malloc(), free()
+using namespace std;
+
+int main(){
+    //printf("guardiamo come funzionano i puntatori\n"); //plain c syntax
+    cout << "guardiamo come funzionano i puntatori" << endl;
+    int a = 2;
+    int* p = &a;
+    printf("a = %d\n", a);
+    printf("&a = %p\n",&a);
+    printf("p = %p\n", p); // the value of p is the address of a
+    //printf("*p = %p\n", *p); // * extracts the value at that address (a)
+    // Pointers to arrays
+    int arr[4] = {10,20, 30, 40};
+    //int* p = arr; // p was already declared. if you declare again it raises error
+    p = arr; // but you can still re-use it
+    for (int i = 0; i < 4; i++){
+        cout << "p + "<< i << " = "  << p+i << endl;  //address of arr[i]
+        cout << "&arr["<< i << "] = "  << &arr[i] << endl;  //address of arr[i]
+        cout << "*(p + "<< i << ")=" << *(p+i) << endl; // element arr[i]
+    }
+    cout << "sizeof(p)= "<< sizeof(p) <<endl; // 8 bytes,
+    //  i.e. the size of a memory address in a 64-bit architecture
+    //
+    cout << "ora guardiamo come si usa malloc" << endl;
+    // malloc() returns a pointer to the allocated address
+    //
+    // dynamic allocation of an int
+    //
+    //
+    int* pp = (int *) malloc(sizeof(int));
+    cout << "pp =" << pp << endl; // the actual address is 64 bits, 
+    // here only 4x9 are shown probabily because the leading bits are zeros
+    cout << "*pp =" << *pp << endl;  // = 0
+    //cout << sizeof(int) << endl; // 4
+    //cout << sizeof(pp) << endl;
+    // filling the address execution time
+    *pp = 2;
+    cout << "After assignation:" << endl << "*pp =" << *pp << endl; 
+    // free the address
+    free(pp);
+    // still, the address may still temporarily contain your variable...
+    // until some other part of the code reclaims it
+    cout << "After free(): " << endl;
+    cout << "pp =" << pp << endl;
+    cout << "*pp =" << *pp << endl; // = 0
+    //
+    //
+    //
+    // dynamic allocation of an array of ints
+    cout << endl << endl << "Dynamic allocation of an array:" <<endl;
+    int arr_size = 5;
+    int* pp_a = (int *) malloc(arr_size * sizeof(int));
+    cout << "pp_a =" << pp_a << endl; // this is the address of the first element
+    for(int i = 0; i< arr_size; i++){
+        // these are two equivalent ways to access the value of the array element:
+        cout << "pp_a[i]= " << pp_a[i]  <<endl; // ! does NOT return an address, but the value contained ...
+        cout << "*(pp_a + i) =" << *(pp_a + i) << endl; // ... exactly like this
+        cout << "&pp_a[i]" << &pp_a[i] << endl;
+        //*pp_a = 5 * i;
+        //pp_a += 1; // increment address
+    }
+    cout << endl << "Now lets fill the array: " << endl;
+    cout << "value\t address" << endl;
+    for(int i = 0; i < arr_size; i++){
+        pp_a[i] = i + 5;
+    }
+    for(int i = 0; i < arr_size; i++){
+       cout << pp_a[i] << "\t" << &pp_a[i] << endl;
+    }
+    //
+    //
+    cout << "Copying blocks of memory: "<< endl;
+    int * pp_b = (int*) malloc(arr_size * sizeof(int)); // you need to allocate memory for pp_b
+    // otherwise you get a segmentation fault (trying to access memory locations that do not exist)
+    cout << "pp_b ="<< pp_b << endl;
+    memcpy(pp_b, pp_a, arr_size * sizeof(int));
+    cout << "value\t address" << endl;
+    for(int i = 0; i < arr_size; i++){
+       cout << pp_b[i] << "\t" << &pp_b[i] << endl;
+    }
+    cout << endl << "Double Pointers: pointers to pointers" << endl;
+    int some_value = 11;
+    int*  r = &some_value; // pointer to a
+    int** s = &r; // pointer to r
+
+    // usage: dynamic allocation of 2d array
+    int rows = 3, cols = 4;
+    int** array = (int**) malloc(rows * sizeof(int*));  // allocate array of pointers
+    for(int i = 0; i < rows; i++){
+        array[i] = (int*) malloc(cols * sizeof(int));   // allocate each row
+    }
+    cout << "row \t col" << endl;
+    for(int i= 0; i < rows; i++){
+        for(int j =0; j < cols; j++){
+            array[i][j] = i*j;
+            cout << i << "\t" << j << "\t" << array[i][j] << endl;
+        }
+    }
+    return(0);
+}
---
+++ cuda-c/cuda_basics.cu
@@ -0,0 +1,66 @@
+/* To sync folder in host with folder in device, use:
+rsync -avz --delete /Users/miriamzara/MCP/cuda-c/ jetson@192.168.55.1:~/cuda_scripts/
+*/
+
+#include <stdio.h>
+#include <iostream>
+using namespace std;
+__global__ void print_int_kernel(int* number);
+__global__ void increment_int_kernel(int* number);
+
+// Main function, entry point of the program
+
+/* In general, if you need to use keyboard
+arguments, you should define it like:
+
+int main(int argc, char **argv){}
+
+Otherwise, simply use:
+
+int main(){}
+*/
+int main(){
+    cout << "Basic memory allocation on GPU." << endl;
+    int* p; // pointer to int (exists on host)
+    // Now we want to fill p with a  memory address of the GPU
+    cudaMalloc((void**)&p, sizeof(int)); 
+    // cudaMalloc expects a valid CPU memory address as first input
+    cout << "p = "<< p << endl; //this is a gpu address
+    //cout << "*p ="<< *p << endl; // if we try to dereference it, 
+    /* it crashes because address does not exist on the host cpu.
+    Output:
+
+    (mcp) jetson@jetson:~/cuda_scripts$ ./a.out
+    Basic memory allocation on GPU.
+    p = 0xf00830000
+    Segmentation fault (core dumped)
+
+    */
+    cout << "Copying data from host to device:" << endl;
+    int a = 10;
+    // cudaMemcpy(device_address, host_address, transfer_type)
+    cudaMemcpy(p, &a, sizeof(int), cudaMemcpyHostToDevice); 
+    print_int_kernel<<<1,1>>>(p);
+    cudaDeviceSynchronize();
+
+    cout << "Incrementing number from the GPU..." << endl;
+    increment_int_kernel<<<1,1>>>(p);
+    cudaDeviceSynchronize();
+    cout << "Copying it back to CPU..." << endl;
+    int result = 0;
+    cudaMemcpy(&result, p, sizeof(int), cudaMemcpyDeviceToHost);
+    cout << "Result is "<< result<< endl;
+    cudaFree(p);
+    return 0;
+}
+
+__global__ void print_int_kernel(int* number){
+    printf("\033[35m"); // changes to bold magenta
+    printf("\n Hello from GPU. I received the number: %d\n", *number); // cout does not work on gpu
+    printf("\n\n");
+    printf("\033[0m"); // reset to black
+}
+
+__global__ void increment_int_kernel(int* number){
+    *number += 1; 
+}
